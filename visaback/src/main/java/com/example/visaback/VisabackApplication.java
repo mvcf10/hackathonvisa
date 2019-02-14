f526e9c08eefcde252b005c9fbe6cb5d438f0aa6
Vinicius Figueiredo <mvcf10@gmail.com>
2019-02-14 14:30:49 -0300

    first commit backend

---
+++ visaback/src/main/java/com/example/visaback/VisabackApplication.java
@@ -0,0 +1,14 @@
+package com.example.visaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisabackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisabackApplication.class, args);
+	}
+
+}
+
